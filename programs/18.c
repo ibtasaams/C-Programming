061c7b4dbb689e39b009d054e49dc81336b56f32
ibtasaams <iabbasi.bee25seecs@seecs.edu.pk>
2025-10-28 01:20:36 +0500

    Add 18.c

---
+++ programs/18.c
@@ -0,0 +1,9 @@
+////Write a program in c to check if a character is vowel or consonant
+#include <stdio.h>
+int main(){
+    char ch;
+    printf("Enter any character: ");
+    scanf("%c",&ch);
+    if(ch)
+return 0;    
+}
